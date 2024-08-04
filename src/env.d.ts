6747a221ad39a0de788c6a8d2ee60a41f814971e
Bart Brusselmans <bart.brusselmans@two-point-o.be>
2024-08-04 14:14:58 +0200

    feat: add declaration file for environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_CF_SPACE_ID: string;
+  readonly PUBLIC_CF_ENVIRONMENT: string;
+  readonly PUBLIC_CF_ACCESS_TOKEN: string;
+  readonly PUBLIC_CF_PREVIEW_TOKEN: string;
+
+  readonly PUBLIC_BGG_USERS: string;
+  readonly PUBLIC_BGG_HOST: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
